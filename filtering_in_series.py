c4909d4312eacb9342e447846d974eec59f5f049
karthik <chukkakarthik1506@gmail.com>
2026-03-27 13:38:50 +0530

    made changes

--- filtering_in_series.py
+++ filtering_in_series.py
@@ -44,8 +44,8 @@ print(s[s.between(20,70)]) # prints true values
 # 5. filtering using where()
 # where is one of most important 
 # keeps all values but replaces non-matching ones with NaN
-
 print(s.where(s>50))
+
 # where() does not modify the original series by default to change the original series 
 # we use inplace if inplace=True it changes the original series else false it does not changes
 # by default it is false
@@ -54,3 +54,13 @@ print(s.where(s>50))
 
 # 6. filtering with mask()
 # this is opposite of where() keeps non-matching ones but remaining are replaced with NaN
+print(s.mask(s>50))
+
+
+# 7. filtering strings(inportant)
+string=pd.Series(['apple','banana','cherry','date'])
+print(string.str.contains('a')) # if a exsist in the string the true else false
+
+# what actually happening in the statement:
+# a pandas series is a colletion of values, not a single value because contains() is a
+# string method, not a series method string.contains('a') X does not work
