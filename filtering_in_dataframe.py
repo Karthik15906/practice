2530ccfa1b42f8f6521564e2e1547a2892a2e844
karthik <chukkakarthik1506@gmail.com>
2026-03-28 12:02:28 +0530

    made changes

--- filtering_in_dataframe.py
+++ filtering_in_dataframe.py
@@ -12,3 +12,23 @@ print(df[['name','marks']])
 '''Boolean Filtering (Core Concept)'''
 
 print(df[df['marks']>50]) # df['marks']>50 -> give true/false
+# pandas keeps only true values
+
+'''Multiple Conditions'''
+
+# filtering based on multiple conditions
+
+print(df[(df['marks']>50) & (df['marks']<90)])
+print(df[(df['marks'] > 80) | (df['name'] == 'rahul')])
+print(df[~(df['marks'] > 50)])
+
+# Use &, |, ~ (not and, or, not)
+
+'''Using .loc[] for Filtering'''
+
+print(df.loc[df['marks']>50])
+# same result but more flexible
+
+# to select specific column 
+print(df.loc[df['marks']>50,['name']])
+
