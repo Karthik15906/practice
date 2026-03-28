0a37b79d42a35f80be3fa1aec34aa565f2cce44e
karthik <chukkakarthik1506@gmail.com>
2026-03-28 13:09:16 +0530

    made changes

--- filtering_in_dataframe.py
+++ filtering_in_dataframe.py
@@ -5,6 +5,9 @@
 import pandas as pd
 df=pd.read_csv('pandas-practice\\data_dataframe\\data1_dataframe.csv')
 
+# The DataFrames object has a method called info(), that gives you more information about the data set.
+print(df.info())
+
 # this just selects a cloumn, not filtering rows
 print(df['marks'])
 print(df[['name','marks']])
@@ -31,4 +34,36 @@ print(df.loc[df['marks']>50])
 
 # to select specific column 
 print(df.loc[df['marks']>50,['name']])
+
+'''Using .iloc[] (Index-based)'''
+
+# df.iloc[rows, columns]
+
+print(df.iloc[0:3])
+
+# Slicing rows + Selecting columns,Slice both rows and columns
+print(df.iloc[0:3,0:2])
+
+# Slicing rows + Multiple columns
+print(df.iloc[0:3,[0,2]])
+
+# Single row + Slice columns
+print(df.iloc[2,0:3])
+
+# List of rows + Slice columns
+print(df.iloc[[0,2,4],1:3])
+
+'''df.iloc[rows, columns]'''
+
+'''uery() Method (Very Clean Syntax)'''
+
+print(df.query('marks >50'))
+print(df.query("marks > 50 and marks < 90"))
+
+# Looks like SQL → easier to read
+
+'''isin() (Filter by Multiple Values)'''
+
+print(df[df['name'].isin(['karthik','anu'])])
+# Useful when checking multiple values
 
